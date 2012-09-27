fe4d1f26756611d0d7bf2b311a82c2e729ba3aee
Eduardo Dias <edurdias@gmail.com>
2012-09-27 17:24:51 -0400

    Automating

---
+++ .nuget/RunningObjects/RunningObjectsBootstrapper.cs.pp
@@ -0,0 +1,23 @@
+﻿using RunningObjects.MVC;
+using RunningObjects.MVC.Persistence;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.RunningObjectsBootstrapper), "Start")]
+namespace $rootnamespace$
+{
+    public static class RunningObjectsBootstrapper
+    {
+        public static void Start()
+        {
+            RunningObjectsSetup.Initialize(config =>
+            {
+                //var referenceType = typeof(MyDomainClass);
+                //config.Mapping.MapAssembly
+                //(
+                //    referenceType.Assembly, 
+                //    referenceType.Namespace, 
+                //    type => new EntityFrameworkRepository(type, new MyDbContext())
+                //);
+            });
+        }
+    }
+}
