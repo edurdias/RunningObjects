89737a304fff541227276bcd1649d792e7b1cf18
Eduardo Dias <edurdias@gmail.com>
2012-09-26 20:52:02 -0400

    Preparing automated build and packaging, done.

--- MVC/Properties/AssemblyInfo.cs
+++ MVC/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Running Objects")]
+[assembly: AssemblyTitle("Running Objects for ASP.NET MVC")]
 [assembly: AssemblyDescription(@"Running Objects is a framework that allows you to use simple OO concepts, focus on your business domain and do not waste time with areas that are not relevant to your application. You will build better software, with less time and with no pain.
 
 It’s built on top of ASP.NET MVC using a refined mix of concepts such as Domain-Driven Design, Naked Objects, Object-Oriented UI and Design Patterns. Basically, everything that is necessary to let you aggregate the highest business value in the shortest time to your application.")]
@@ -12,6 +12,7 @@ It’s built on top of ASP.NET MVC using a refined mix of concepts such as Domai
 [assembly: AssemblyTrademark("Eduardo Dias")]
 [assembly: AssemblyCulture("en-US")]
 [assembly: AssemblyVersion("1.1.1.*")]
+
 
 [assembly: ComVisible(false)]
 [assembly: Guid("6011298d-4be1-4a52-805d-73bec3bd2daa")]
